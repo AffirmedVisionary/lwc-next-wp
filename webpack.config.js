f6b965b68dbccc1fe17839d7f9df18f822160a65
AffirmedVisionary <iam@charlenegrant.co.uk>
2021-05-10 08:05:37 +0100

    moved the array of services

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const svgToMiniDataURI = require('mini-svg-data-uri');
+const path = require('path');
+
+
 module.exports = {
     module: {
       rules: [
@@ -8,7 +12,47 @@ module.exports = {
                 name: 'images/[name].[ext]'
             }
         },
-      ],
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                generator: (content) => svgToMiniDataURI(content.toString()),
+                encoding: 'utf8',
+              },
+            },
+            {
+              loader: 'svg-inline-loader',
+              options: {
+                removeTags: true
+              }
+            }
+          ],
+      },
+    ],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+     {
+       test: /\.(csv|tsv)$/i,
+       use: ['csv-loader'],
+     },
+     {
+       test: /\.xml$/i,
+       use: ['xml-loader'],
+     },
+    ],
     },
 };
   
